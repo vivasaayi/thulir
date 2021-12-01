8cc1bea54e5bcc1ab8df6f4780bda0d18d51f48d
Rajan Panneer Selvam <vivasaayi@live.com>
2021-12-01 08:13:35 -0500

    Added ReactUI

---
+++ Thulir.ReactUI/ClientApp/src/components/LandsatDataSetBrowser.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class LandsatDataSetBrowser extends Component {
+    static displayName = LandsatDataSetBrowser.name;
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello from Landsat DataSet Browser</h1>
+            </div>
+        );
+    }
+}
